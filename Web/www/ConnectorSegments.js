9cd12f9501890b79d90af0dba3be8b47cf6cb002
matrefeytontias <mattias@refeyton.fr>
2017-05-22 22:52:30 +0200

    Nettoyage de code, ajout de commentaires

--- Web/www/ConnectorSegments.js
+++ Web/www/ConnectorSegments.js
@@ -1,12 +1,12 @@
 // Requires three.js
 
+// Trace une ligne entre un objet parent et une cible (Vector3 target)
 function ConnectorSegments(parent, target = new THREE.Vector(0, 0, 0), color = 0xffffff)
 {
     this.parent = parent;
     this.target = target;
     var g = new THREE.Geometry();
     g.vertices.push(new THREE.Vector3());
-    // g.vertices.push(new THREE.Vector3(0, target.y - parent.position.y, target.z - parent.position.z));
     g.vertices.push(target.clone().sub(parent.position));
     var m = new THREE.LineBasicMaterial({color:color});
     this.object = new THREE.Line(g, m);
@@ -14,9 +14,6 @@ function ConnectorSegments(parent, target = new THREE.Vector(0, 0, 0), color = 0
 
 ConnectorSegments.prototype.update = function()
 {
-    // this.object.geometry.vertices[1].y = this.target.y - this.parent.position.y; 
-    // this.object.geometry.vertices[1].y = this.target.z - this.parent.position.z;
-    // this.object.geometry.vertices[2] = this.target.clone().sub(this.parent.position);
     this.object.geometry.vertices[1] = this.target.clone().sub(this.parent.position);
     this.object.geometry.verticesNeedUpdate = true;
 }
