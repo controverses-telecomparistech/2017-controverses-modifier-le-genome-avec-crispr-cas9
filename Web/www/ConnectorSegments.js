d3c214fa7b6e020b4c529af77a29a27377bc83c7
matrefeytontias <mattias@refeyton.fr>
2017-05-22 22:30:32 +0200

    Implémentation de la rotation des boîtes autour du modèle d'ADN

--- Web/www/ConnectorSegments.js
+++ Web/www/ConnectorSegments.js
@@ -6,16 +6,17 @@ function ConnectorSegments(parent, target = new THREE.Vector(0, 0, 0), color = 0
     this.target = target;
     var g = new THREE.Geometry();
     g.vertices.push(new THREE.Vector3());
-    g.vertices.push(new THREE.Vector3(0, target.y - parent.position.y, target.z - parent.position.z));
+    // g.vertices.push(new THREE.Vector3(0, target.y - parent.position.y, target.z - parent.position.z));
     g.vertices.push(target.clone().sub(parent.position));
     var m = new THREE.LineBasicMaterial({color:color});
     this.object = new THREE.Line(g, m);
 }
 
-ConnectorSegments.prototype.update = function ()
+ConnectorSegments.prototype.update = function()
 {
-    var a = this.object.geometry.vertices;
-    a[1].y = target.y - parent.y;
-    a[2].set(target.x - parent.position.x, target.y - parent.position.y, target.z - parent.position.z);
+    // this.object.geometry.vertices[1].y = this.target.y - this.parent.position.y; 
+    // this.object.geometry.vertices[1].y = this.target.z - this.parent.position.z;
+    // this.object.geometry.vertices[2] = this.target.clone().sub(this.parent.position);
+    this.object.geometry.vertices[1] = this.target.clone().sub(this.parent.position);
     this.object.geometry.verticesNeedUpdate = true;
 }
