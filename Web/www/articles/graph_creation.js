b1f42f18e76f12857d415b095bb3656fe5415a5a
matrefeytontias <mattias@refeyton.fr>
2017-06-09 02:22:20 +0200

    Ajout de la découverte des noeuds des arbres par clics

--- Web/www/articles/graph_creation.js
+++ Web/www/articles/graph_creation.js
@@ -1,39 +1,47 @@
 function createGraph(file, container)
 {
-  var s = new sigma({
-  container: container,
-  renderer: {
-  container: document.getElementById(container),
-  type: 'webgl'
-  },
-  settings: {
-  minNodeSize: 8,
-  maxNodeSize: 16
-  }
-  });
-  
-  sigma.parsers.gexf('data/' + file, s, function ()
-    {
-      s.refresh();
-      // On affiche les divs quand on clique sur les noeuds
-      s.bind("clickNode",
-        function (e)
-        {
-          currentDiv = $("#" + e.data.node.id);
-          if(currentDiv)
-          {
-            currentDiv.addClass("current");
-          }
+    var s = new sigma({
+        container: container,
+        renderer: {
+            container: document.getElementById(container),
+            type: 'webgl'
+        },
+        settings: {
+            minNodeSize: 8,
+            maxNodeSize: 16
         }
-      ).bind("click", function ()
+    });
+    
+    sigma.parsers.gexf('data/' + file, s, function ()
         {
-          if(currentDiv)
-          {
-            currentDiv.removeClass("current");
-            currentDiv = undefined;
-          }
+            s.refresh();
+            // On affiche les divs quand on clique sur les noeuds
+            s.bind("clickNode", function (e)
+                {
+                    currentDiv = $("#" + e.data.node.id);
+                    if(currentDiv[0])
+                    {
+                        currentDiv.addClass("current");
+                    }
+                    else // s'il n'y a pas d'article, on révèle les voisins
+                    {
+                        var n = s.graph.neighbors(e.data.node.id);
+                        for(var nodeId in n)
+                        {
+                            s.graph.invertNodeHidden(n[nodeId].id);
+                        }
+                        s.refresh();
+                    }
+                }
+            ).bind("click", function ()
+                {
+                    if(currentDiv)
+                    {
+                        currentDiv.removeClass("current");
+                        currentDiv = undefined;
+                    }
+                }
+            );
         }
-      );
-    }
-  );
+    );
 }
